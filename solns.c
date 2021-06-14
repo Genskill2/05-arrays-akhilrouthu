26103451f29884524112302e7ebca56b00c8854a
akhilrouthu <85018230+akhilrouthu@users.noreply.github.com>
2021-06-14 13:54:42 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -43,7 +43,7 @@ int min(int arr[], int size)
      float total = 0;
      for(int i =0; i<size; i++)
      {
-       total+= arr[i]
+       total+= arr[i];
      }
      avg = total/size;
    return avg;
